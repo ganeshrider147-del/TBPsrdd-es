240bc9a6635ba3bbee9ca2976cdc8b82329ce180
SiddhardhaVandrangi <wwww.siddhardha@gmail.com>
2026-06-28 21:00:29 +0530

    Configure automatic migrations execution in Procfile and wsgi.py on deployment container startup

--- SRDD/backend/SmartRoadBackend/backend/wsgi.py
+++ SRDD/backend/SmartRoadBackend/backend/wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
+
+try:
+    from django.core.management import call_command
+    print("Executing automatic database migrations in WSGI application...")
+    call_command('migrate', interactive=False)
+    print("Database migrations applied successfully!")
+except Exception as e:
+    print(f"Error running programmatic database migrations: {e}")
